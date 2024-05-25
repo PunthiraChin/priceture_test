0688649fe51c12d4e38b5740b2d68c6dfb6f18c9
PunthiraChin <punthira.chino@gmail.com>
2024-05-25 23:10:07 +0700

    move coingecko api to backend with hardcode

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
+
+require("dotenv").config();
 
 const app = express();
+let COINGECKO_API_KEY = process.env.COINGECKO_API_KEY;
 
 app.use(
   cors({
@@ -11,10 +15,17 @@ app.use(
   })
 );
 app.use(express.json());
-app.get("/", (req, res) => {
-  console.log("Successfully connect to backend");
-  res.send("hey");
-  //   res.status(200).json({ message: "successful connect to backend" });
+app.get("/", async (req, res) => {
+  console.log("Your request is being processed");
+  // Replace selectedAsset with data from request body
+  const selectedAsset = "bitcoin";
+  const response = await axios.get(
+    `https://api.coingecko.com/api/v3/simple/price?ids=${selectedAsset}&vs_currencies=usd&x_cg_demo_api_key=${COINGECKO_API_KEY}`
+  );
+  //   console.log(response.data[selectedAsset].usd);
+  const assetPrice = response.data[selectedAsset].usd;
+  //   console.log(assetPrice);
+  res.json({ assetPrice });
 });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
